4879c7b9d987e1d1232938d211a7c72fb508fd50
= <=>
2023-10-29 17:22:55 +0530

    Project setup

--- src/app/layout.js
+++ src/app/layout.js
@@ -21,17 +21,17 @@ export default function RootLayout({ children }) {
         <script
           src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"
           integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+"
-          crossorigin="anonymous"
-        ></script>
+          crossOrigin="anonymous"
+        />
         <link
           href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
           rel="stylesheet"
           integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
-          crossorigin="anonymous"
-        ></link>
+          crossOrigin="anonymous"
+        />
       </head>
       <body className={inter.className}>
-        <div><AppLayout /></div>
+        <AppLayout />
         {children}
       </body>
     </html>
